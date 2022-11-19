274e0bf42ac8ea408b49bac7481065a23baa164e
Jaico1 <jaimecoronado07@gmail.com>
2022-11-18 23:08:55 -0500

    create and getters

--- contracts/purchaseContract.sol
+++ contracts/purchaseContract.sol
@@ -22,30 +22,40 @@ contract purchaseContract {
 
     struct Client{
         uint idclient;
+        string name;
     }
-    Comic[] public comics;
+    Comic[] public comics; 
+    uint public nextidComic;
     Purchase[] public purchases;
+    uint public nextIdPurchase;
     Client[] public clients;
+    uint public nextIdClients;
     
-    function createComic(int price) public{
-
+    function createComic (int _price, string memory _name_comic, string memory _author, uint  _publish_date, int  _isbn, Client memory _owner) public{
+        comics.push(Comic(nextidComic++, _price,_name_comic, _author, _publish_date, _isbn, _owner));
     }
 
-    function getComic(uint id) public{
-
+    function getComic(uint id) public view returns (Comic memory) {
+        return comics[id];
     }
 
-    function getPurchase(uint id) public{
-        
+    function createPurchase(uint _purchase_date, Comic memory _item, Client memory _owner, Client memory _buyer) public {
+        purchases.push(Purchase(nextIdPurchase++, _purchase_date, _item, _owner, _buyer));
     }
 
-    function getClient(uint id) public{
-
+    function createClient (string memory _name) public{
+        clients.push(Client(nextIdClients++, _name));
     }
 
-    function geta(uint id) public view returns (Comic memory){
-        return comics[0];
+    function getPurchase(uint id) public view returns (Purchase memory){
+        return purchases[id];
     }
+
+    function getClient(uint id) public view returns (Client memory){
+        return clients[id];
+    }
+
+
     //function
     
     
